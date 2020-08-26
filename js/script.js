c0f5a9833520753af8ddd37d20423b7987947632
Gabriele-Musumeci <musumeci.gabriele@hotmail.com>
2020-08-26 16:24:23 +0200

    Finish e pronto per abbellimento :)

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
-alert("Sto Cazzo !");
+// FizzBuzz
+// Scrivi un programma che stampi i numeri da 1 a 100,
+// ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+
+
+var fizz = "Fizz";
+var buzz = "Buzz";
+
+var numero = 1;
+
+while (numero <= 100) {
+  if ((numero / 3 == parseInt(numero / 3)) && (numero / 5 == parseInt(numero / 5))) {
+    document.getElementById("lista-numeri").innerHTML += "<li>" + fizz + buzz + "<li>";
+    console.log(fizz + buzz);
+  }
+  else if (numero / 5 == parseInt(numero / 5)) {
+    document.getElementById("lista-numeri").innerHTML += "<li>" + buzz + "<li>";
+    console.log(buzz);
+  }
+  else if (numero / 3 == parseInt(numero / 3)) {
+  document.getElementById("lista-numeri").innerHTML += "<li>" + fizz + "<li>";
+  console.log(fizz);
+  }
+  else{
+    document.getElementById("lista-numeri").innerHTML += "<li>" + numero + "<li>";
+    console.log(numero);
+  }
+
+  numero++;
+}
